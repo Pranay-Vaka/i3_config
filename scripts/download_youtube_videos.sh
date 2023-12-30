673c673fb7e1159d69dfe4280d549e7d07b4d866
Pranay-Vaka <vakapranaya05@gmail.com>
2023-12-30 00:49:09 +0000

    testing things

--- scripts/download_youtube_videos.sh
+++ scripts/download_youtube_videos.sh
@@ -2,5 +2,8 @@
 
 # Retrieve the URL from the clipboard using xclip
 url=$(xclip -selection clipboard -o)
-music_path = $(~/Media/Music/)
-yt-dlp -x -f mp3 -P ~/Media/Music/ -o '%(title)s.%(ext)s' $url
+
+music_path="$HOME/Media/Music/"
+music_path="${music_path/\~/$HOME}"
+
+yt-dlp -x --audio-format mp3 -P "$music_path" -o '%(title)s.%(ext)s' "$url"
